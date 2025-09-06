3fdece61ffa6a8f7de9dbc102cc0ba0861c31299
Michelle <mpuschkarow@web.de>
2025-09-06 11:26:56 +0200

    image path changed

--- src/components/Dishes.tsx
+++ src/components/Dishes.tsx
@@ -2,7 +2,7 @@ const Dishes = () => {
     return ( 
         <div className="dish-card pd-16-24">
             <h2>Hauptgerichte</h2>
-            <img src="/statics/images/imgMain0.png" alt="Main dish" />
+            <img src="statics/images/imgMain0.png" alt="Main dish" />
             <div className="add-to-cart-wrapper">
                 <h3>Pizza Sandwich</h3>
                 <button className="btn dark-btn">+</button>
